eecb7d46f2a265ce799fcf02051f3e6e15573fe0
Vanessa Ferreira <vanessa2209vieira@gmail.com>
2022-06-01 18:46:33 -0300

    commit do curso

---
+++ sintaxe basica Javascript/aula02 - variáveis/script.js
@@ -0,0 +1,96 @@
+//tipos primitivos 
+
+//boolean
+/*var VouF = false
+console.log(typeof(VouF))
+
+//number
+var numeroQualquer = 1
+console.log(typeof(numeroQualquer))
+
+//string
+var nome = 'diego'
+console.log(typeof(nome))*/
+
+//var variavel = 'diego'
+//variavel = 'lucas'
+//console.log(variavel)
+
+//let variavel2 = 'diego'
+//variavel2 = 'lucas' 
+//console.log(variavel2)
+
+//const constante = 'diego'
+//console.log(constante) 
+
+/*var escopoGlobal = 'global'
+console.log(escopoGlobal)
+
+function escopoLocal (){
+    let escopoLocalInterno = 'local'
+        console.log(escopoLocalInterno)
+    }
+escopoLocal()*/
+
+//atribuicao
+//var atribuicao = 'diego'
+
+//comparacao
+// var comparacao = '0'== 0
+// console.log(comparacao)
+
+// comparacao identica
+// var comparacaoIdentica = '0'=== 0
+// console.log(comparacaoIdentica)
+
+// adicao
+// var adicao = 1 + 1
+// console.log(adicao)
+
+// subtracao
+// var subtracao = 2 - 1
+// console.log(subtracao)
+
+// multiplicacao
+// var multiplicacao = 2 * 3
+// console.log(multiplicacao)
+
+// divisaoReal
+// var divisaoReal = 6 / 2
+// console.log(divisaoReal)
+
+// divisaoInteira
+// var divisaoInteira = 5 % 2
+// console.log(divisaoInteira)
+
+// potenciacao
+// var potenciacao = 2 ** 10
+// console.log(potenciacao)
+
+// maiorQue
+// var maiorQue = 5 > 2
+// console.log(maiorQue)
+
+// menorQue
+// var menorQue = 5 < 2
+// console.log(menorQue)
+
+// maiorOuIgual
+// var maiorOuIgual = 5 >= 2
+// console.log(maiorOuIgual)
+
+// menorOuIgual
+// var menorOuIgual = 5 <= 2
+// console.log(menorOuIgual)
+
+// e
+// var e = true && true
+// console.log(e)
+
+// ou
+// var ou = false || false
+// console.log(ou)
+
+// nao
+// var nao = !true
+// console.log(nao)
